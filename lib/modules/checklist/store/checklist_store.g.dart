00b5d37606cedcf25685993255f15b6bd8e799fc
Matheus Albuquerque <matheusalbuquerque@Matheuss-MacBook-Pro.local>
2025-11-11 00:20:10 -0300

    Este commit adiciona primeira estrutura do fluxo de checklist

---
+++ lib/modules/checklist/store/checklist_store.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'checklist_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$ChecklistStore on _ChecklistStore, Store {
+  late final _$_ChecklistStoreActionController = ActionController(
+    name: '_ChecklistStore',
+    context: context,
+  );
+
+  @override
+  void startChecklist() {
+    final _$actionInfo = _$_ChecklistStoreActionController.startAction(
+      name: '_ChecklistStore.startChecklist',
+    );
+    try {
+      return super.startChecklist();
+    } finally {
+      _$_ChecklistStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
